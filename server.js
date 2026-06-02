3b36b7e28ca3ec6491cc3b6e893bc067b6505b11
Sangzz09 <tranminhsang14102009@gmail.com>
2026-06-02 12:57:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const API_URL =
-  "
-https://demo7892.fun/history/getLastResult?gameId=ktrng_3986&size=100&tableId=398625062021&curPage=1";
+  "https://demo7892.fun/history/getLastResult?gameId=ktrng_3986&size=100&tableId=398625062021&curPage=1";
 
 // ─── Fetch data from source ───────────────────────────────────────────────────
 async function fetchData() {
